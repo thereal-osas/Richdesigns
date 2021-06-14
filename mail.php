fd1b8308d3cb0b46a9bfec5ac7a93fc8ff897f27
thereal-osas <abrahamosas3@gmail.com>
2021-06-13 18:04:56 -0700

    quick update
    contact-form testing 2

--- mail.php
+++ mail.php
@@ -1,17 +1,19 @@
-<?php
-//get data from form  
-$name = $_POST['name'];
-$email= $_POST['email'];
-$title= $_POST['subject'];
-$message= $_POST['message'];
-$to = "richdesignsglobal@gmail.com";
-$subject = "Mail From richdesigns";
-$txt ="Name = ". $name . "\r\n  Email = " . $email . "\r\n Subject = " . $title . "\r\n Message =" . $message;
-$headers = "From: noreply@richdesigns.com" . "\r\n" .
-"CC: somebodyelse@example.com";
-if($email!=NULL){
-    mail($to,$subject,$txt,$headers);
-}
-//redirect
-header("Location:thankyou.html");
+<?php 
+    $name = $_POST['name'];
+    $visitor_email= $_POST['email'];
+    $title= $_POST['subject'];
+    $message= $_POST['message'];
+
+    $email_form = 'noreply@richdesigns.com';
+    $email_subject = 'New Form Submission';
+    $email_body = 'User Name ='. $name . '\n'.
+                    'User Email ='. $visitor_email . '\n'.
+                      'User Subject ='. $title . '\n'.
+                        'User Message ='. $message;
+    $to = "richdesignsglobal@gmail.com";
+    $headers = "From: $email_form\r\n;"
+    $headers = "Reply-To:$visitor_email\r\n;"
+        mail($to,$email_subject,$email_body,$headers);
+        header("Location:thankyou.html");
+
 ?>
